71e024daf81e1f7ef86e8e1917480a4176f74319
iillyashenko <iillyashenko@ptsecurity.com>
2024-04-11 20:17:00 +0300

    #1104 add RabbitMQ

--- mq/rabbitmq.go
+++ mq/rabbitmq.go
@@ -23,11 +23,11 @@ func (r *RabbitMQ) Close() {
 	r.conn.Close()
 }
 
-func (r *RabbitMQ) RabbitQueueDeclare(qname string) error {
-	_, err := r.ch.QueueDeclare(qname, false, false, false, false, nil)
+func (r *RabbitMQ) RabbitQueueDeclare(qname string, durable bool, autoDelete bool, exclusive bool, noWait bool, args amqp.Table) error {
+	_, err := r.ch.QueueDeclare(qname, durable, autoDelete, exclusive, noWait, args)
 	return err
 }
 
-func (r *RabbitMQ) ChannelPublish(exchange string, key string, mendatory, immadiate bool, msg amqp.Publishing) error {
-	return r.ch.Publish(exchange, key, mendatory, immadiate, msg)
+func (r *RabbitMQ) ChannelPublish(exchange string, key string, mandatory, immediate bool, msg amqp.Publishing) error {
+	return r.ch.Publish(exchange, key, mandatory, immediate, msg)
 }
